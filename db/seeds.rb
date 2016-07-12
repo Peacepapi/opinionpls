53aeac183266387467e8e176a5d867eb41c42c33
Pier Yos <khmaipapi@yahoo.com>
2016-07-12 10:32:08 -0400

    change seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,36 +14,37 @@ Category.create(name: 'Movies')
 Category.create(name: 'TV Shows')
 Category.create(name: 'Music')
 Category.create(name: 'Fitness')
+Category.create(name: 'Other')
 
-Question.create(title: "What was your favorite cartoon?", user_id: 1, category_id:5, choices_attributes:[
-	{option: "Lorem ipsum t, consectetur adipiscing elit"},
-	{option: "Lorem iectetur adipiscing elit"},
-	{option: "Lorem ipsum dolor sit adipiscing elit"}])
-Question.create(title: "How many pairs of shoes do you own?", user_id: 2, category_id:2, choices_attributes:[
-	{option: "Lorem ipsum t, consectetur adipiscing elit"},
-	{option: "Lorem iectetur adipiscing elit"},
-	{option: "Lorem ipsum dolor sit adipiscing elit"}])
-Question.create(title: "Is it harder to exercise more or eat healthier?", user_id: 3, category_id:7, choices_attributes:[
-	{option: "Lorem ipsum t, consectetur adipiscing elit"},
-	{option: "Lorem iectetur adipiscing elit"},
-	{option: "Lorem ipsum dolor sit adipiscing elit"}])
-Question.create(title: "What is one thing that you would like to change about the world?", user_id: 4, category_id:6, choices_attributes:[
-	{option: "Lorem ipsum t, consectetur adipiscing elit"},
-	{option: "Lorem iectetur adipiscing elit"},
-	{option: "Lorem ipsum dolor sit adipiscing elit"}])
-Question.create(title: "If you were the opposite gender for one day, what would you do?", user_id: 6, category_id:2, choices_attributes:[
-	{option: "Lorem ipsum t, consectetur adipiscing elit"},
-	{option: "Lorem iectetur adipiscing elit"},
-	{option: "Lorem ipsum dolor sit adipiscing elit"}])
-Question.create(title: "If you knew you only had 24 hours left to live, what would you do?", user_id: 5, category_id:3, choices_attributes:[
-	{option: "Lorem ipsum t, consectetur adipiscing elit"},
-	{option: "Lorem iectetur adipiscing elit"},
-	{option: "Lorem ipsum dolor sit adipiscing elit"}])
-Question.create(title: "If you could only eat one food for the rest of your life, what would it be?", user_id: 3, category_id:4, choices_attributes:[
-	{option: "Lorem ipsum t, consectetur adipiscing elit"},
-	{option: "Lorem iectetur adipiscing elit"},
-	{option: "Lorem ipsum dolor sit adipiscing elit"}])
-Question.create(title: "Are you a pessimist or an optimist?", user_id: 4, category_id:1, choices_attributes:[
-	{option: "Lorem ipsum t, consectetur adipiscing elit"},
-	{option: "Lorem iectetur adipiscing elit"},
-	{option: "Lorem ipsum dolor sit adipiscing elit"}])
+# Question.create(title: "What was your favorite cartoon?", user_id: 1, category_id:5, choices_attributes:[
+# 	{option: "Lorem ipsum t, consectetur adipiscing elit"},
+# 	{option: "Lorem iectetur adipiscing elit"},
+# 	{option: "Lorem ipsum dolor sit adipiscing elit"}])
+# Question.create(title: "How many pairs of shoes do you own?", user_id: 2, category_id:2, choices_attributes:[
+# 	{option: "Lorem ipsum t, consectetur adipiscing elit"},
+# 	{option: "Lorem iectetur adipiscing elit"},
+# 	{option: "Lorem ipsum dolor sit adipiscing elit"}])
+# Question.create(title: "Is it harder to exercise more or eat healthier?", user_id: 3, category_id:7, choices_attributes:[
+# 	{option: "Lorem ipsum t, consectetur adipiscing elit"},
+# 	{option: "Lorem iectetur adipiscing elit"},
+# 	{option: "Lorem ipsum dolor sit adipiscing elit"}])
+# Question.create(title: "What is one thing that you would like to change about the world?", user_id: 4, category_id:6, choices_attributes:[
+# 	{option: "Lorem ipsum t, consectetur adipiscing elit"},
+# 	{option: "Lorem iectetur adipiscing elit"},
+# 	{option: "Lorem ipsum dolor sit adipiscing elit"}])
+# Question.create(title: "If you were the opposite gender for one day, what would you do?", user_id: 6, category_id:2, choices_attributes:[
+# 	{option: "Lorem ipsum t, consectetur adipiscing elit"},
+# 	{option: "Lorem iectetur adipiscing elit"},
+# 	{option: "Lorem ipsum dolor sit adipiscing elit"}])
+# Question.create(title: "If you knew you only had 24 hours left to live, what would you do?", user_id: 5, category_id:3, choices_attributes:[
+# 	{option: "Lorem ipsum t, consectetur adipiscing elit"},
+# 	{option: "Lorem iectetur adipiscing elit"},
+# 	{option: "Lorem ipsum dolor sit adipiscing elit"}])
+# Question.create(title: "If you could only eat one food for the rest of your life, what would it be?", user_id: 3, category_id:4, choices_attributes:[
+# 	{option: "Lorem ipsum t, consectetur adipiscing elit"},
+# 	{option: "Lorem iectetur adipiscing elit"},
+# 	{option: "Lorem ipsum dolor sit adipiscing elit"}])
+# Question.create(title: "Are you a pessimist or an optimist?", user_id: 4, category_id:1, choices_attributes:[
+# 	{option: "Lorem ipsum t, consectetur adipiscing elit"},
+# 	{option: "Lorem iectetur adipiscing elit"},
+# 	{option: "Lorem ipsum dolor sit adipiscing elit"}])
